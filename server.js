421fe638a50a8986af831588997270b4dd666ecc
seshanpv <seshanpv@gmail.com>
2016-12-08 16:41:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/js/jquery.pagepiling.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'jquery.pagepiling.js'));
+});
+
+app.get('/css/jquery.pagepiling.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'jquery.pagepiling.css'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
